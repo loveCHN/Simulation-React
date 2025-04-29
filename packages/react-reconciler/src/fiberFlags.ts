f62d2b406ba7a6fd05f368e1f23b1f8057f4d5c3
wangqingyun <2510061498@qq.com>
2025-04-29 17:22:50 +0800

    ✨ feat(playground): 更新playground启动脚本以包含生产构建步骤，并添加VSCode调试配置文件；修正fiberFlags中的标志位定义

--- packages/react-reconciler/src/fiberFlags.ts
+++ packages/react-reconciler/src/fiberFlags.ts
@@ -1,11 +1,11 @@
 export type Flags = number;
 
-export const NoFlags = 0b000001;
+export const NoFlags = 0b0000000;
 
-export const Placement = 0b000010;
+export const Placement = 0b0000001;
 
-export const Update = 0b000100;
+export const Update = 0b0000010;
 
-export const ChildDeletion = 0b001000;
+export const ChildDeletion = 0b0000100;
 
 export const MutationMask = Placement | Update | ChildDeletion;
